42585b9d428512bc6023e17b2f606903bcd6ea03
Naveen Gopala <gsnavin@gmail.com>
2018-09-13 14:51:38 -0700

    Fix the JWT version requirement

--- docusign_esign.gemspec
+++ docusign_esign.gemspec
@@ -27,7 +27,7 @@ Gem::Specification.new do |s|
   #s.license     = "Apache 2.0"
   s.required_ruby_version = ">= 2.1"
 
-  s.add_dependency 'jwt', '~> 2.0', '>= 2.0.0'
+  s.add_dependency 'jwt', '~> 1.0', '>= 1.5.2'
 
   s.add_runtime_dependency 'typhoeus', '~> 1.0', '>= 1.0.1'
   s.add_runtime_dependency 'json', '~> 1.8', '>= 1.8.3'
--- tests/spec/unit_tests_spec.rb
+++ tests/spec/unit_tests_spec.rb
@@ -108,24 +108,24 @@ describe 'DocuSign Ruby Client Tests' do
 
   before(:all) do
     # run before each test
-    $username = 'gsnavin@gmail.com'
-    $password = 'gopala'
-    $integrator_key = '***REMOVED***'
+    $username = 'node_sdk@mailinator.com'
+    $password = '{PASSWORD}'
+    $integrator_key = 'ae30ea4e-xxxx-xxxx-xxxx-fcb57d2dc4df'
 
     $auth_header = '{"Username": "%s", "Password": "%s", "IntegratorKey": "%s"}' %[$username, $password, $integrator_key]
 
-    $recipient_email = "naveen.gopala@docusign.com"
-    $recipient_name = "Naveen Gopala"
+    $recipient_email = "node_sdk@mailinator.com"
+    $recipient_name = "Pat Developer"
 
     # Required for embedded signing url
     $client_user_id = '1234'
-    $return_url = 'https://www.docusign.com/devcenter'
+    $return_url = 'https://developers.docusign.com/'
     $authentication_method = 'email'
 	    
-    $template_id = ''
+    $template_id = 'cf2a46c2-xxxx-xxxx-xxxx-752547b1a419'
     $envelope_id = nil
 
-    $base_url = nil
+    $base_url = 'https://demo.docusign.net/restapi'
     $account_id = nil
   end
 
